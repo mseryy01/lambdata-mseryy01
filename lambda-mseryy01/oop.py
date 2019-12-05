0e2da8dd9ce72f26cbec6583efa2fdda35ba6ae8
Mikhail Seryy <mseryy01@users.noreply.github.com>
2019-12-05 18:20:16 -0400

    test

--- lambda-mseryy01/oop.py
+++ lambda-mseryy01/oop.py
@@ -1,20 +1,13 @@
 class Person:
-    ### Namee and age tracking ###
+    ### Name and Age tracking#
 
-    def __init__(self, personName, personAge):
-        self.name = personName
-        self.age = personAge
+    def __init__(self, Name, Age):
+        self.name = Name
+        self.age = Age
 
-    def showName(self):
-        print("My name is" + self.name)
+    def show_name(self):
+        return print('My name is ' + self.name)
 
-    def showAge(self):
+    def show_age(self):
         years = self.age + 1
-        print("I am " + years + " years old")
-
-
-person1 = Person("Mike", 32)
-person2 = Person("Sara", 41)
-
-person1.showAge()
-person2.showName()
+        return print("I will be " + years + "years old next year")
--- lambda-mseryy01/oop_test.py
+++ lambda-mseryy01/oop_test.py
@@ -1,14 +1,13 @@
-#import the unit test package and functions we want to test out
 import unittest
-from oop import show_age show_name
+from oop import show_age, show_name
 
 class AgeTest(unittest.TestCase):
     """Obligatory docstring, test show_age and show_name functions!"""
     def test_age32(self):
-        self.assertEqual(shpw_age(32), 33)
+        self.assertEqual(show_age(32), 33)
 
     def test_name(self):
-        self.assertEqual(show_name(Mike),'Mike')
+        self.assertEqual(show_name('Mike'),'Mike')
 
 if __name__ == '__main__':
     unittest.main()
